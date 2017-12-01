ce141079d834c41fcd03c9e906f1684b9706b20c
ibnuridho <jakrhifa@gmail.com>
2017-12-01 21:42:38 +0700

    add lib/main, read_xml_bc23, dir xml/bc23

---
+++ lib/main.php
@@ -0,0 +1,105 @@
+<?php
+
+function insertRefernce($table, $arrData, $allField = false){
+    global $db;
+
+    $arrData = array_change_key_case($arrData,CASE_UPPER);
+    $arrData = array_map('trim', $arrData);
+    $arrData = array_map('strtoupper', $arrData);
+    $sqlTBL = 'SHOW FIELDS FROM ' . $table;
+    $datTBL = $db->query($sqlTBL);
+    while($row = $datTBL->fetch_assoc()){
+        $arrTBL[] = $row;
+    }
+    // return arrTBL;
+    $whrTBL = '';
+    $data   = '';
+    foreach ($arrTBL as $aTBL) {
+        $aTBL = array_change_key_case($aTBL, CASE_UPPER);
+        $aTBL = array_map('strtoupper' , $aTBL);
+        $trueData = false;
+        if ($aTBL['EXTRA'] != 'AUTO_INCREMENT') {
+            if($allField){
+                $data[$aTBL['FIELD']] = "'" . $arrData[$aTBL['FIELD']] . "'";
+                $trueData = true;
+            }else{
+                if(isset($arrData[$aTBL['FIELD']])){
+                    $data[$aTBL['FIELD']] = "'" . $arrData[$aTBL['FIELD']] . "'";
+                    $trueData = true;
+                }
+            }
+            if($trueData){
+                $type = explode('(', $aTBL['TYPE']);
+                switch ($type[0]){
+                    case'DATE':
+                        $strDate = $arrData[$aTBL['FIELD']];
+                        if(strlen($strDate) == 8){
+                            $date = [ substr($strDate, 0, 4), substr($strDate, 4, 2), substr($strDate, 6, 2) ];
+                        }else{
+                            $date = explode('-', $strDate);
+                        }
+                        if(checkdate($date[1], $date[2], $date[0])){
+                            $data[$aTBL['FIELD']] = "'" . $date[0] . '-' . $date[1] . '-' . $date[2] . "'";
+                        }else{
+                            $data[$aTBL['FIELD']] = "'" . '0000-00-00' . "'";
+                        }
+                        
+                        break;
+                    case 'DATETIME':
+                        list($date_, $time_) = explode(' ', $arrData[$aTBL['FIELD']]);
+                        $date = explode('-', $date_);
+                        if(checkdate($date[1], $date[0], $date[2])){
+                            $data[$aTBL['FIELD']] = "'" . $date[2] . '-' . $date[1] . '-' . $date[0] . ' ' . $time_ . "'";
+                        }else{
+                            // $data[$aTBL['FIELD']] = 'NULL';
+                            $data[$aTBL['FIELD']] = "'" . '0000-00-00' . "'";
+                        }
+                        break;
+                    case'DOUBLE':
+                        $data[$aTBL['FIELD']] = floatval($data[$aTBL['FIELD']]);
+                        break;
+                    case'INT':
+                        $data[$aTBL['FIELD']] = str_replace(',', '', $arrData[$aTBL['FIELD']]);
+                        break;
+                }
+            }
+        }
+        #find key
+        if ($aTBL['KEY'] == 'PRI') {
+            $whrTBL[$aTBL['FIELD']] = $arrData[$aTBL['FIELD']];
+            if($trueData){
+                $whrTBL[$aTBL['FIELD']] = $data[$aTBL['FIELD']];
+            }
+        }
+
+    }
+
+    foreach($whrTBL as $key => $value) $dataWhere[] = "$key = $value";
+    $dataWhere = implode(" AND ", $dataWhere);
+
+    $sqlCek = "SELECT * FROM $table WHERE " . $dataWhere;
+    $datCek = $db->query($sqlCek);
+
+    if ($datCek->num_rows == 0) {
+        $columns = implode(", ",array_keys($data));
+        $values  = implode(", ", array_values($data));
+
+        $sqlIns = "INSERT INTO $table ($columns) VALUES ($values)";
+        $exec = $db->query($sqlIns);
+        
+    } else {
+        // $data = array_diff($data, $whrTBL);
+        foreach($data as $key => $value) $dataUpd[] = "$key=$value";
+        $dataUpd = implode(", ", $dataUpd);
+
+        $sqlUpd = "UPDATE $table SET $dataUpd WHERE " . $dataWhere;
+        $exec = $db->query($sqlUpd);
+        
+    }
+
+    if($exec){
+        return [$table => true];
+    }else{
+        return [$table => false];
+    }
+}
---
+++ read_xml_bc23.php
@@ -0,0 +1,245 @@
+<?php
+include('lib/database.php');
+include('lib/main.php');
+
+// READ XML
+$bc23_xml_dir = $_SERVER['DOCUMENT_ROOT']."/pibbc23interface/xml/bc23/".date('Ymd');
+$filter = "/*.xml";
+$xmlFiles  = array_slice(glob($bc23_xml_dir . $filter, GLOB_BRACE),0,1000);
+
+foreach($xmlFiles as $file){
+	isset($dataHeader);
+	isset($dataItem);
+	isset($dataDoc);
+	isset($dataItemDoc);
+	isset($dataCon);
+	isset($dataKms);
+	isset($dataPgt);
+	isset($dataTrf);
+	isset($insertData);
+
+	$xmlFilePath   	= realpath($file);
+	$xmlContent		= file_get_contents($xmlFilePath);
+	$xmlArr		 	= simplexml_load_string($xmlContent);
+
+	foreach ($xmlArr->HEADER as $xmlHeader) {
+		$dataHeader = [
+			'KODE_TRADER' => $kode_trader = ($xmlHeader->KODE_TRADER) ? (string)$xmlHeader->KODE_TRADER : "0",
+			'CAR' => ($xmlHeader->CAR) ? (string)$xmlHeader->CAR : "-",
+			'KDKPBC' => ($xmlHeader->KDKPBC) ? (string)$xmlHeader->KDKPBC : "",
+			'TUJUAN' => ($xmlHeader->TUJUAN) ? (string)$xmlHeader->TUJUAN : "",
+			'PASOKNAMA' => ($xmlHeader->PASOKNAMA) ? (string)$xmlHeader->PASOKNAMA : "",
+			'PASOKALMT' => ($xmlHeader->PASOKALMT) ? (string)$xmlHeader->PASOKALMT : "",
+			'PASOKNEG' => ($xmlHeader->PASOKNEG) ? (string)$xmlHeader->PASOKNEG : "",
+			'USAHAID' => ($xmlHeader->USAHAID) ? (string)$xmlHeader->USAHAID : "",
+			'USAHANPWP' => ($xmlHeader->USAHANPWP) ? (string)$xmlHeader->USAHANPWP : "",
+			'KDTPB' => ($xmlHeader->KDTPB) ? (string)$xmlHeader->KDTPB : "",
+			'REGISTRASI' => ($xmlHeader->REGISTRASI) ? (string)$xmlHeader->REGISTRASI : "",
+			'USAHANAMA' => ($xmlHeader->USAHANAMA) ? (string)$xmlHeader->USAHANAMA : "",
+			'USAHAALMT' => ($xmlHeader->USAHAALMT) ? (string)$xmlHeader->USAHAALMT : "",
+			'USAHASTATUS' => ($xmlHeader->USAHASTATUS) ? (string)$xmlHeader->USAHASTATUS : "",
+			'APIKD' => ($xmlHeader->APIKD) ? (string)$xmlHeader->APIKD : "",
+			'APINO' => ($xmlHeader->APINO) ? (string)$xmlHeader->APINO : "",
+			'PPJKID' => ($xmlHeader->PPJKID) ? (string)$xmlHeader->PPJKID : "",
+			'PPJKNPWP' => ($xmlHeader->PPJKNPWP) ? (string)$xmlHeader->PPJKNPWP : "",
+			'PPJKNAMA' => ($xmlHeader->PPJKNAMA) ? (string)$xmlHeader->PPJKNAMA : "",
+			'PPJKALMT' => ($xmlHeader->PPJKALMT) ? (string)$xmlHeader->PPJKALMT : "",
+			'PPJKNO' => ($xmlHeader->PPJKNO) ? (string)$xmlHeader->PPJKNO : "",
+			'PPJKTG' => ($xmlHeader->PPJKTG) ? (string)$xmlHeader->PPJKTG : "",
+			'INDID' => ($xmlHeader->INDID) ? (string)$xmlHeader->INDID : "",
+			'INDNPWP' => ($xmlHeader->INDNPWP) ? (string)$xmlHeader->INDNPWP : "",
+			'INDNAMA' => ($xmlHeader->INDNAMA) ? (string)$xmlHeader->INDNAMA : "",
+			'INDALMT' => ($xmlHeader->INDALMT) ? (string)$xmlHeader->INDALMT : "",
+			'INDAPIKD' => ($xmlHeader->INDAPIKD) ? (string)$xmlHeader->INDAPIKD : "",
+			'INDAPINO' => ($xmlHeader->INDAPINO) ? (string)$xmlHeader->INDAPINO : "",
+			'MODA' => ($xmlHeader->MODA) ? (string)$xmlHeader->MODA : "",
+			'ANGKUTNAMA' => ($xmlHeader->ANGKUTNAMA) ? (string)$xmlHeader->ANGKUTNAMA : "",
+			'ANGKUTNO' => ($xmlHeader->ANGKUTNO) ? (string)$xmlHeader->ANGKUTNO : "",
+			'ANGKUTFL' => ($xmlHeader->ANGKUTFL) ? (string)$xmlHeader->ANGKUTFL : "",
+			'PELBKR' => ($xmlHeader->PELBKR) ? (string)$xmlHeader->PELBKR : "",
+			'PELMUAT' => ($xmlHeader->PELMUAT) ? (string)$xmlHeader->PELMUAT : "",
+			'PELTRANSIT' => ($xmlHeader->PELTRANSIT) ? (string)$xmlHeader->PELTRANSIT : "",
+			'BC23NO' => ($xmlHeader->BC23NO) ? (string)$xmlHeader->BC23NO : "",
+			'BC23TG' => ($xmlHeader->BC23TG) ? (string)$xmlHeader->BC23TG : "",
+			'KDKPBCBONGKAR' => ($xmlHeader->KDKPBCBONGKAR) ? (string)$xmlHeader->KDKPBCBONGKAR : "",
+			'KDKPBCAWAS' => ($xmlHeader->KDKPBCAWAS) ? (string)$xmlHeader->KDKPBCAWAS : "",
+			'DOKTUPKD' => ($xmlHeader->DOKTUPKD) ? (string)$xmlHeader->DOKTUPKD : "",
+			'DOKTUPNO' => ($xmlHeader->DOKTUPNO) ? (string)$xmlHeader->DOKTUPNO : "",
+			'DOKTUPTG' => ($xmlHeader->DOKTUPTG) ? (string)$xmlHeader->DOKTUPTG : "",
+			'POSNO' => ($xmlHeader->POSNO) ? (string)$xmlHeader->POSNO : "",
+			'POSSUB' => ($xmlHeader->POSSUB) ? (string)$xmlHeader->POSSUB : "",
+			'POSSUBSUB' => ($xmlHeader->POSSUBSUB) ? (string)$xmlHeader->POSSUBSUB : "",
+			'TMPTBN' => ($xmlHeader->TMPTBN) ? (string)$xmlHeader->TMPTBN : "",
+			'KDVAL' => ($xmlHeader->KDVAL) ? (string)$xmlHeader->KDVAL : "",
+			'NDPBM' => ($xmlHeader->NDPBM) ? (string)$xmlHeader->NDPBM : "",
+			'NILINV' => ($xmlHeader->NILINV) ? (string)$xmlHeader->NILINV : "",
+			'FREIGHT' => ($xmlHeader->FREIGHT) ? (string)$xmlHeader->FREIGHT : "",
+			'BTAMBAHAN' => ($xmlHeader->BTAMBAHAN) ? (string)$xmlHeader->BTAMBAHAN : "",
+			'DISKON' => ($xmlHeader->DISKON) ? (string)$xmlHeader->DISKON : "",
+			'KDASS' => ($xmlHeader->KDASS) ? (string)$xmlHeader->KDASS : "",
+			'ASURANSI' => ($xmlHeader->ASURANSI) ? (string)$xmlHeader->ASURANSI : "",
+			'KDHRG' => ($xmlHeader->KDHRG) ? (string)$xmlHeader->KDHRG : "",
+			'FOB' => ($xmlHeader->FOB) ? (string)$xmlHeader->FOB : "",
+			'CIF' => ($xmlHeader->CIF) ? (string)$xmlHeader->CIF : "",
+			'CIFRP' => ($xmlHeader->CIFRP) ? (string)$xmlHeader->CIFRP : "",
+			'BRUTO' => ($xmlHeader->BRUTO) ? (string)$xmlHeader->BRUTO : "",
+			'NETTO' => ($xmlHeader->NETTO) ? (string)$xmlHeader->NETTO : "",
+			'JMCONT' => ($xmlHeader->JMCONT) ? (string)$xmlHeader->JMCONT : "",
+			'JMBRG' => ($xmlHeader->JMBRG) ? (string)$xmlHeader->JMBRG : "",
+			'TGLTTD' => ($xmlHeader->TGLTTD) ? (string)$xmlHeader->TGLTTD : "",
+			'KOTATTD' => ($xmlHeader->KOTATTD) ? (string)$xmlHeader->KOTATTD : "",
+			'NAMATTD' => ($xmlHeader->NAMATTD) ? (string)$xmlHeader->NAMATTD : "",
+			'CONTAKPERSON' => ($xmlHeader->CONTAKPERSON) ? (string)$xmlHeader->CONTAKPERSON : "",
+			'NOPHONE' => ($xmlHeader->NOPHONE) ? (string)$xmlHeader->NOPHONE : "",
+			'NOFAX' => ($xmlHeader->NOFAX) ? (string)$xmlHeader->NOFAX : "",
+			'EMAIL' => ($xmlHeader->EMAIL) ? (string)$xmlHeader->EMAIL : "",
+			'KDFAS' => ($xmlHeader->KDFAS) ? (string)$xmlHeader->KDFAS : "",
+			'NOSKEPFAS' => ($xmlHeader->NOSKEPFAS) ? (string)$xmlHeader->NOSKEPFAS : "",
+			'TGLSKEPFAS' => ($xmlHeader->TGLSKEPFAS) ? (string)$xmlHeader->TGLSKEPFAS : "",
+		];	
+		$insertData[] = insertRefernce('t_bc23hdr', $dataHeader);
+	}
+
+	foreach ($xmlArr->DETAIL->DOCUMENTS as $xmlDocs) {
+		foreach ($xmlDocs->DOCUMENT as $xmlDoc) {
+			$dataDoc = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'DOKKD' => ($xmlDoc->DOKKD) ? (string)$xmlDoc->DOKKD : "",
+				'DOKNO' => ($xmlDoc->DOKNO) ? (string)$xmlDoc->DOKNO : "",
+				'DOKTG' => ($xmlDoc->DOKTG) ? (string)$xmlDoc->DOKTG : "",
+				'SERIDOK' => ($xmlDoc->SERIDOK) ? (string)$xmlDoc->SERIDOK : "",
+			];
+			$insertData[] = insertRefernce('t_bc23dok', $dataDoc);
+		}
+	}
+
+	foreach ($xmlArr->DETAIL->ITEMS as $xmlItems) {
+		foreach ($xmlItems->ITEM as $xmlItem) {
+			$dataItem = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'SERIAL' => ($xmlItem->SERIAL) ? (string) : "",
+				'JNSBARANGDTL' => ($xmlItem->JNSBARANGDTL) ? (string) : "",
+				'NOHS' => ($xmlItem->NOHS) ? (string) : "",
+				'BRGURAI' => ($xmlItem->BRGURAI) ? (string) : "",
+				'MERK' => ($xmlItem->MERK) ? (string) : "",
+				'TIPE' => ($xmlItem->TIPE) ? (string) : "",
+				'UKURAN' => ($xmlItem->UKURAN) ? (string) : "",
+				'SPFLAIN' => ($xmlItem->SPFLAIN) ? (string) : "",
+				'KDBRG' => ($xmlItem->KDBRG) ? (string) : "",
+				'KEMASJN' => ($xmlItem->KEMASJN) ? (string) : "",
+				'KEMASJM' => ($xmlItem->KEMASJM) ? (string) : "",
+				'KDFASDTL' => ($xmlItem->KDFASDTL) ? (string) : "",
+				'BRGASAL' => ($xmlItem->BRGASAL) ? (string) : "",
+				'DNILINV' => ($xmlItem->DNILINV) ? (string) : "",
+				'DCIF' => ($xmlItem->DCIF) ? (string) : "",
+				'KDSAT' => ($xmlItem->KDSAT) ? (string) : "",
+				'JMLSAT' => ($xmlItem->JMLSAT) ? (string) : "",
+				'NETTODTL' => ($xmlItem->NETTODTL) ? (string) : "",
+				'HRGSAT' => ($xmlItem->HRGSAT) ? (string) : "",
+				'KDSKEMATARIF' => ($xmlItem->KDSKEMATARIF) ? (string) : "",
+				'STATUS' => ($xmlItem->STATUS) ? (string) : "",
+			];
+			$insertData[] = insertRefernce('t_bc23dtl', $dataItem);
+		}
+	}
+
+	foreach ($xmlArr->DETAIL->ITEMDOCUMENTS as $xmlItemDocs) {
+		foreach ($xmlItemDocs->ITEMDOCUMENT as $xmlItemDoc) {
+			$dataItemDoc = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'SERIBRG' => ($xmlItemDoc->SERIBRG) ? (string)$xmlItemDoc->SERIBRG : "",
+				'SERIDOK' => ($xmlItemDoc->SERIDOK) ? (string)$xmlItemDoc->SERIDOK : "",
+			];
+		}
+		$insertData[] = insertRefernce('t_bc23dtldok', $dataItemDoc);
+	}
+
+	foreach ($xmlArr->DETAIL->CONTAINERS as $xmlCons) {
+		foreach ($xmlCons->CONTAINER as $xmlCon) {
+			$dataCon = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'CONTNO' => ($xmlCon->CONTNO) ? (string)$xmlCon->CONTNO : "",
+				'CONTUKUR' => ($xmlCon->CONTUKUR) ? (string)$xmlCon->CONTUKUR : "",
+				'CONTTIPE' => ($xmlCon->CONTTIPE) ? (string)$xmlCon->CONTTIPE : "",
+				'KETERANGAN' => ($xmlCon->KETERANGAN) ? (string)$xmlCon->KETERANGAN : "",
+			];
+			$insertData[] = insertRefernce('t_bc23con', $dataCon);
+		}
+	}
+
+	foreach ($xmlArr->DETAIL->PACKAGES as $xmlKmss) {
+		foreach ($xmlKmss->PACKAGE as $xmlKms) {
+			$dataKms = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'JNKEMAS' => ($xmlKms->JNKEMAS) ? (string)$xmlKms->JNKEMAS : "",
+				'JMKEMAS' => ($xmlKms->JMKEMAS) ? (string)$xmlKms->JMKEMAS : "",
+				'MERKKEMAS' => ($xmlKms->MERKKEMAS) ? (string)$xmlKms->MERKKEMAS : "",
+			];
+			$insertData[] = insertRefernce('t_bc23kms', $dataKms);
+		}
+	}
+
+	foreach ($xmlArr->DETAIL->CHARGES as $xmlPgts) {
+		foreach ($xmlPgts->CHARGE as $xmlPgt) {
+			$dataPgt = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'KDBEBAN' => ($xmlPgt->KDBEBAN) ? (string)$xmlPgt->KDBEBAN : "",
+				'KDFASIL' => ($xmlPgt->KDFASIL) ? (string)$xmlPgt->KDFASIL : "",
+				'NILBEBAN' => ($xmlPgt->NILBEBAN) ? (string)$xmlPgt->NILBEBAN : "",
+			];
+			$insertData[] = insertRefernce('t_bc23pgt', $dataPgt);
+		}
+	}
+
+	foreach ($xmlArr->DETAIL->ITEMTARIFS as $xmlTrfs) {
+		foreach ($xmlTrfs->TARIF as $xmlTrf) {
+			$dataTrf = [
+				'KODE_TRADER' => $kode_trader,
+				'CAR'	=> $car,
+				'SERIBRG' =>($xmlTrf->SERIBRG) ? (string)$xmlTrf->SERIBRG : "",
+				'KDTRPBM' =>($xmlTrf->KDTRPBM) ? (string)$xmlTrf->KDTRPBM : "",
+				'KDSATBM' =>($xmlTrf->KDSATBM) ? (string)$xmlTrf->KDSATBM : "",
+				'TRPBM' =>($xmlTrf->TRPBM) ? (string)$xmlTrf->TRPBM : "",
+				'TRPPPN' =>($xmlTrf->TRPPPN) ? (string)$xmlTrf->TRPPPN : "",
+				'TRPPBM' =>($xmlTrf->TRPPBM) ? (string)$xmlTrf->TRPPBM : "",
+				'TRPPPH' =>($xmlTrf->TRPPPH) ? (string)$xmlTrf->TRPPPH : "",
+				'KDTRPBMAD' =>($xmlTrf->KDTRPBMAD) ? (string)$xmlTrf->KDTRPBMAD : "",
+				'TRBMAD' =>($xmlTrf->TRBMAD) ? (string)$xmlTrf->TRBMAD : "",
+				'FLBMADS' =>($xmlTrf->FLBMADS) ? (string)$xmlTrf->FLBMADS : "",
+				'KDTRPBMTP' =>($xmlTrf->KDTRPBMTP) ? (string)$xmlTrf->KDTRPBMTP : "",
+				'TRPBMTP' =>($xmlTrf->TRPBMTP) ? (string)$xmlTrf->TRPBMTP : "",
+				'FLBMTPS' =>($xmlTrf->FLBMTPS) ? (string)$xmlTrf->FLBMTPS : "",
+				'KDTPRBMIM' =>($xmlTrf->KDTPRBMIM) ? (string)$xmlTrf->KDTPRBMIM : "",
+				'TRPBMIM' =>($xmlTrf->TRPBMIM) ? (string)$xmlTrf->TRPBMIM : "",
+				'FLBIMPS' =>($xmlTrf->FLBIMPS) ? (string)$xmlTrf->FLBIMPS : "",
+				'KDTRPBMPB' =>($xmlTrf->KDTRPBMPB) ? (string)$xmlTrf->KDTRPBMPB : "",
+				'TRPBMPB' =>($xmlTrf->TRPBMPB) ? (string)$xmlTrf->TRPBMPB : "",
+				'FLBMPBS' =>($xmlTrf->FLBMPBS) ? (string)$xmlTrf->FLBMPBS : "",
+				'KDFASBM' =>($xmlTrf->KDFASBM) ? (string)$xmlTrf->KDFASBM : "",
+				'KDFASPPN' =>($xmlTrf->KDFASPPN) ? (string)$xmlTrf->KDFASPPN : "",
+				'KDFASPPH' =>($xmlTrf->KDFASPPH) ? (string)$xmlTrf->KDFASPPH : "",
+				'KDPASPPNBM' =>($xmlTrf->KDPASPPNBM) ? (string)$xmlTrf->KDPASPPNBM : "",
+				'FASBM' =>($xmlTrf->FASBM) ? (string)$xmlTrf->FASBM : "",
+				'FASPPN' =>($xmlTrf->FASPPN) ? (string)$xmlTrf->FASPPN : "",
+				'FASPPH' =>($xmlTrf->FASPPH) ? (string)$xmlTrf->FASPPH : "",
+				'FASPPNBM' =>($xmlTrf->FASPPNBM) ? (string)$xmlTrf->FASPPNBM : "",
+				'JMLSAT' =>($xmlTrf->JMLSAT) ? (string)$xmlTrf->JMLSAT : "",
+				'KDCUK' =>($xmlTrf->KDCUK) ? (string)$xmlTrf->KDCUK : "",
+				'KDTRPCUK' =>($xmlTrf->KDTRPCUK) ? (string)$xmlTrf->KDTRPCUK : "",
+				'KDSATCUK' =>($xmlTrf->KDSATCUK) ? (string)$xmlTrf->KDSATCUK : "",
+				'TRPCUK' =>($xmlTrf->TRPCUK) ? (string)$xmlTrf->TRPCUK : "",
+				'KDFASCUK' =>($xmlTrf->KDFASCUK) ? (string)$xmlTrf->KDFASCUK : "",
+				'FASCUK' =>($xmlTrf->FASCUK) ? (string)$xmlTrf->FASCUK : "",
+			];
+			$insertData[] = insertRefernce('t_bc23trf', $dataTrf);
+		}
+	}
+}
+
+$db->close();
