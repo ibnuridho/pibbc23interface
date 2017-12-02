e1f87f97214e61d211bda68f344cfcfc280bd114
ibnuridho <jakrhifa@gmail.com>
2017-12-02 21:53:21 +0700

    modified - remove variable for sqlCek

--- read_msaccess_pib.php
+++ read_msaccess_pib.php
@@ -2,7 +2,6 @@
 error_reporting(ALL);
 include('lib/database.php');
 include('lib/main.php');
-global $db;
 
 $dbName = $_SERVER["DOCUMENT_ROOT"] . "/pibbc23interface/msaccess/dbPIB.mdb";
 if (!file_exists($dbName)) {
@@ -15,10 +14,6 @@ try {
     $result = $dbA->query($sqlRes);
 
 	while ($row = $result->fetch()) {
-		$CAR = "'".$row['CAR']."'";
-		$RESKD = "'".$row['RESKD']."'"; 
-		$RESTG = "'".$row['RESTG']."'"; 
-		$RESWK = "'".$row['RESWK']."'";
 		isset($data);
 		$data = [
 			'CAR' => $row['CAR'], 'RESKD' => $row['RESKD'], 'RESTG' => $row['RESTG'], 'RESWK' => $row['RESWK'], 'DOKRESNO' => $row['DOKRESNO'], 'DOKRESTG' => $row['DOKRESTG'], 'KPBC' => $row['KPBC'], 'PIBNO' => $row['PIBNO'], 'PIBTG' => $row['PIBTG'], 'KDGUDANG' => $row['KDGUDANG'], 'PEJABAT1' => $row['PEJABAT1'], 'Nip1' => $row['Nip1'], 'JABATAN1' => $row['JABATAN1'], 'PEJABAT2' => $row['PEJABAT2'], 'Nip2' => $row['Nip2'], 'JATUHTEMPO' => $row['JATUHTEMPO'], 'KOMTG' => $row['KOMTG'], 'KOMWK' => $row['KOMWK'], 'DesKripsi' => $row['DesKripsi'], 'dibaca' => $row['dibaca'], 'JmKemas' => $row['JmKemas'], 'NoKemas' => $row['NoKemas'], 'NPWPImp' => $row['NPWPImp'], 'NamaImp' => $row['NamaImp'], 'AlamatImp' => $row['AlamatImp'], 'IDPPJK' => $row['IDPPJK'], 'NamaPPJK' => $row['NamaPPJK'], 'AlamatPPJK' => $row['AlamatPPJK'], 'KodeBill' => $row['KodeBill'], 'TanggalBill' => $row['TanggalBill'], 'TanggalJtTempo' => $row['TanggalJtTempo'], 'TanggalAju' => $row['TanggalAju'], 'TotalBayar' => $row['TotalBayar'], 'Terbilang' => $row['Terbilang'],
