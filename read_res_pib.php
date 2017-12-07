30e3bb0e64c7881c9db4f80ab95099bc39855893
ibnuridho <jakrhifa@gmail.com>
2017-12-07 19:37:14 +0700

    add another res for pib

--- read_res_pib.php
+++ read_res_pib.php
@@ -21,10 +21,61 @@ try {
 		
 	    $insertData[] = insertRefernce('t_bc20res', $data);
 	    set_logs('RESPIB', $row['CAR'], 'Load Response', 0);
+
+	    $sqlResConR = "SELECT * FROM tblPibConR WHERE CAR = '" . $row['CAR'] . "'";
+	    $resConR = $dbA->query($sqlResConR);
+
+		while ($rowConR = $resConR->fetch()) {
+			isset($dataConR);
+			$dataConR = [
+				'CAR' => $rowConR['CAR'], 'RESKD' => $rowConR['RESKD'], 'CONTNO' => $rowConR['CONTNO'], 'CONTUKUR' => $rowConR['CONTUKUR'], 'CONTTIPE' => $rowConR['CONTTIPE'],
+			];
+
+			$insertData[] = insertRefernce('t_bc20conr', $dataConR);
+	    	set_logs('RESPIBCONR', $rowConR['CAR'], 'Load Response', 0);
+		}
+
+		$sqlResBill = "SELECT * FROM tblPibResBill WHERE CAR = '" . $row['CAR'] . "'";
+	    $resBill = $dbA->query($sqlResBill);
+
+		while ($rowResBill = $resBill->fetch()) {
+			isset($dataResBill);
+			$dataResBill = [
+				'CAR' => $rowResBill['CAR'], 'KODEBILL' => $rowResBill['KODEBILL'], 'ResTg' => $rowResBill['ResTg'], 'ResWk' => $rowResBill['ResWk'], 'Seri' => $rowResBill['Seri'], 'Akun' => $rowResBill['Akun'], 'NPWP' => $rowResBill['NPWP'], 'Nilai' => $rowResBill['Nilai'],
+			];
+
+			$insertData[] = insertRefernce('tblPibResBill', $dataResBill);
+	    	set_logs('RESPIBCONR', $rowResBill['CAR'], 'Load Response', 0);
+		}
+
+		$sqlResNpbl = "SELECT * FROM t_bc20ResNPBL WHERE CAR = '" . $row['CAR'] . "'";
+	    $resNpbl = $dbA->query($sqlResNpbl);
+
+		while ($rowResNpbl = $resNpbl->fetch()) {
+			isset($dataResNpbl);
+			$dataResNpbl = [
+				'CAR' => $rowResNpbl['CAR'], 'ResKd' => $rowResNpbl['ResKd'], 'ResTg' => $rowResNpbl['ResTg'], 'ResWk' => $rowResNpbl['ResWk'], 'Serial' => $rowResNpbl['Serial'], 'BrgUrai' => $rowResNpbl['BrgUrai'], 'Ketentuan' => $rowResNpbl['Ketentuan'], 'Pemberitahuan' => $rowResNpbl['Pemberitahuan'], 'Penetapan' => $rowResNpbl['Penetapan'],
+			];
+
+			$insertData[] = insertRefernce('t_bc20ResNPBL', $dataResNpbl);
+	    	set_logs('RESPIBCONR', $rowResNpbl['CAR'], 'Load Response', 0);
+		}
+
+		$sqlResNpd = "SELECT * FROM t_bc20ResNPD WHERE CAR = '" . $row['CAR'] . "'";
+	    $resNpd = $dbA->query($sqlResNpd);
+
+		while ($rowResNpd = $resNpd->fetch()) {
+			isset($dataResNpd);
+			$dataResNpd = [
+				'CAR' => $rowResNpd['CAR'], 'ResTg' => $rowResNpd['ResTg'], 'ResWk' => $rowResNpd['ResWk'], 'Seri' => $rowResNpd['Seri'], 'UrDok' => $rowResNpd['UrDok'], 'Nilai' => $rowResNpd['Nilai'],
+			];
+
+			$insertData[] = insertRefernce('t_bc20ResNPD', $dataResNpd);
+	    	set_logs('RESPIBCONR', $rowResNpd['CAR'], 'Load Response', 0);
+		}
+
 	}
 	
-	print_r($insertData);
-
 }
 catch (PDOException $e) {
     echo $e->getMessage();
